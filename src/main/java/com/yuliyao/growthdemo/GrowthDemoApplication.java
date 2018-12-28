2ab7cb0aa2f28d7a797fa141a3e8571c277d4853
yuliyao <myloveyuvip@qq.com>
2018-12-29 00:45:13 +0800

    Initial commit

---
+++ src/main/java/com/yuliyao/growthdemo/GrowthDemoApplication.java
@@ -0,0 +1,14 @@
+package com.yuliyao.growthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthDemoApplication.class, args);
+    }
+
+}
+
